2cbef5aacd8ae1f630857c9fa794bfbee76967fc
Hoang Dao <daohm@mail.uc.edu>
2018-10-09 16:30:59 -0400

    added trapping rain water

--- 42_trapping_rain_water.py
+++ 42_trapping_rain_water.py
@@ -14,21 +14,48 @@
 
 
 # O(n) DP
+# def trapping_rain_water(height):
+#     ans = 0
+#     n = len(height)
+#     left_max = [0] * n
+#     right_max = [0] * n
+#
+#     left_max[0] = height[0]
+#     right_max[0] = height[n - 1]
+#     for i in range(1, n):
+#         left_max[i] = max(left_max[i - 1], height[i])
+#     for i in reversed(range(n - 1)):
+#         right_max[i] = max(right_max[i + 1], height[i])
+#
+#     for i in range(1, n - 1):
+#         ans += min(left_max[i], right_max[i]) - height[i]
+#
+#     return ans
+
+# O(n)
 def trapping_rain_water(height):
+    if not height:
+        return 0
+
     ans = 0
-    n = len(height)
-    left_max = [0] * n
-    right_max = [0] * n
+    left, right = 0, len(height) - 1
+    left_max, right_max = 0, 0
+    while left < right:
+        if height[left] < height[right]:
+            if height[left] >= left_max:
+                left_max = height[left]
+            else:
+                ans += left_max - height[left]
 
-    left_max[0] = height[0]
-    right_max[0] = height[n - 1]
-    for i in range(1, n):
-        left_max[i] = max(left_max[i - 1], height[i])
-    for i in reversed(range(n - 1)):
-        right_max[i] = max(right_max[i + 1], height[i])
+            left += 1
 
-    for i in range(1, n - 1):
-        ans += min(left_max[i], right_max[i]) - height[i]
+        else:
+            if height[right] >= right_max:
+                right_max = height[right]
+            else:
+                ans += right_max - height[right]
+
+            right -= 1
 
     return ans
 
